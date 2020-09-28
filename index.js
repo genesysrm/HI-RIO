03235791f2f78e7639750cd83ffeed86c83234cf
Genesys Rondon <genesys.merchan@gmail.com>
2020-09-27 21:35:21 -0300

    Adição dos metodos delete post e put

--- index.js
+++ index.js
@@ -42,52 +42,70 @@ app.get('/listavisitas/:id', async (req, res) => {
 })
 
 //Crear novo destino para lista de
-app.post('/novodestino', async (req, res) => {
-  const body = req.body
-  const novoitem = await visita.create(body)
+//app.post('/listavisitas', async (req, res) => {
+  //const body = req.body
+  //const novoitem = await visita.create(body)
 
-  res.json({ novoitem })
-})
-//API lugares Usando ROUTES ANGULAR 
-//async function getAlllugares(req, res) {
-//const lugarList = await
+  //res.json({ novoitem })
+//})
 
-/*
- 
+app.post('/listavisitas', async (req, res) => {
+  const body = req.body;
+  
+  const novoitem = await visita.create({
+      id_lugar: body.id_lugar,
+      nome_lista: body.nome_lista,
+      visitou_lista: body.visitou_lista,
+      comentarios_lista: body.comentarios_lista,
+      valor_lista: body.valor_lista,
+  });
+  res.json({result: "Nova tarefa ", novoitem })
+});
+
+// DELETE LUGARES METODO DE AULA
+app.delete('/listavisitas/:id', async (req, res) => {
+  const lugaresId = req.params.id
+  const lugar = await visita.findByPk(lugaresId)
+  if (lugar) {
+    await lugar.destroy()
+    res.send({ lugar })
+    res.status(200)
+    res.send({message: 'O lugar foi apagado com exito'})
+  } else {
+    res.status(404)
+    res.send({ message: 'lugar nâo existe' })
+  }
+}) 
 
 // Update task
-app.put('/tasks/:id', async (req, res) => {
+app.put('/listavisitas/:id', async (req, res) => {
   const taskId = req.params.id
   const body = req.body
-  const task = await tasks.findByPk(taskId)
+  const lugares = await visita.findByPk(taskId)
 
-  if (task) {
-    await task.update({ ...body })
-    res.send({ task })
+  if (lugares) {
+    await lugares.update({ 
+      id_lugar: body.id_lugar,
+      nome_lista: body.nome_lista,
+      visitou_lista: body.visitou_lista,
+      comentarios_lista: body.comentarios_lista,
+      valor_lista: body.valor_lista
+    })
+    res.send({lugares})
   } else {
     res.status(404)
-    res.send({ message: 'Task not found' })
+    res.send({ message: 'Destino não encontrado' })
   }
 })
 
-// Delete task
-app.delete('/tasks/:id', async (req, res) => {
-  const taskId = req.params.id
-  const task = await tasks.findByPk(taskId)
 
-  if (task) {
-    await task.destroy()
-    res.send({ task })
-  } else {
-    res.status(404)
-    res.send({ message: 'Task not found' })
-  }
-})
-*/
 app.listen(3000, () => {
   console.log('Iniciando o ExpressJS na porta 3000')
 })
 
 
+//API lugares Usando ROUTES ANGULAR 
+//async function getAlllugares(req, res) {
+//const lugarList = await
 
 
